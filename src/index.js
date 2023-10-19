ef91953a722fd23dd6765f13382108b461846ea9
bwa <40144023+beaesq@users.noreply.github.com>
2023-10-19 23:19:06 +0800

    Add location data getter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('stan loona');
+const API_KEY = '64cbe356cfaf4d1a9bb164616230310';
+const API_URL = 'https://api.weatherapi.com/v1/current.json?';
+
+async function getLocationData(location) {
+    const response = await fetch(API_URL + 'key=' + API_KEY + '&q=' + location, {
+        mode: 'cors'
+    });
+    const locationData = await response.json();
+    console.log(locationData);
+}
