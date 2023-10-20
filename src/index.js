2030ee4dc04000f3585f7cf993f49cf4eece48e0
bwa <40144023+beaesq@users.noreply.github.com>
2023-10-21 02:25:42 +0800

    Add location form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,34 @@ import './css/weather-icons.css';
 import './css/weather-icons.min.css';
 import { getForecast, getConditionIconClass } from './weather';
 
+function displayForm() {
+    let form = document.createElement('form');
+    form.setAttribute('id', 'location-form');
+
+    let input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'location');
+    input.setAttribute('name', 'location');
+    form.appendChild(input);
+
+    let button = document.createElement('button');
+    button.setAttribute('type', 'submit')
+    button.innerHTML = 'Search';
+    form.appendChild(button);
+
+    document.body.appendChild(form);
+
+    const locationForm = document.querySelector('#location-form');
+    locationForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let location = locationForm.elements["location"].value.replace(/ /g,"+");
+        console.log(getForecast(location));
+    });
+}
 
 getForecast('new+york');
 
-document.body.innerHTML = `<i class="wi ${getConditionIconClass(1003, true)}"></i>`;
+displayForm();
+
+// document.body.innerHTML = `<i class="wi ${getConditionIconClass(1003, true)}"></i>`;
