b28a177603508b077df44d21f50259da838ebdf6
bwa <40144023+beaesq@users.noreply.github.com>
2023-10-21 02:00:37 +0800

    Split weather functions into separate module

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    weather: './src/weather.js',
+  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
