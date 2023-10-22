88cfa7b624138603d3fc1999f4615f5d479a4503
bwa <40144023+beaesq@users.noreply.github.com>
2023-10-22 22:56:26 +0800

    Add forecast update

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ function updateDisplay(weather) {
     console.log(weather);
 
     updateCurrentDisplay(weather);
+    updateForecastDisplay(weather);
 
 }
 
@@ -43,5 +44,24 @@ function updateCurrentDisplay(weather) {
     document.querySelector('#precipitation').innerHTML = weather.current.precip_mm + 'mm';
 }
 
+function updateForecastDisplay(weather, days = 3) {
+    for (let index = 0; index < days; index++) {
+        // loops through the forecast displays `forecast-${index}`
+        const day = `forecast-${index}-`;
 
-// document.body.innerHTML = `<i class="wi ${getConditionIconClass(1003, true)}"></i>`;
+        document.querySelector(`#${day}date`).innerHTML = weather.forecast[index].date.toLocaleString('en-GB', {hourCycle: 'h12', hour12: true, weekday:'short', month: 'short', day: 'numeric', timeZone: weather.location.tz});
+        document.querySelector(`#${day}moonphase`).setAttribute('class', `wi ${getMoonPhaseIconClass(weather.forecast[index].moon_phase)}`);
+
+        document.querySelector(`#${day}condition-icon`).setAttribute('class', `wi ${getConditionIconClass(weather.forecast[index].condition.code)}`);
+        document.querySelector(`#${day}condition-text`).innerHTML = weather.forecast[index].condition.text;
+
+        document.querySelector(`#${day}maxtemp`).innerHTML = isFahrenheit ? weather.forecast[index].maxtemp_f : weather.forecast[index].maxtemp_c;
+        document.querySelector(`#${day}mintemp`).innerHTML = isFahrenheit ? weather.forecast[index].mintemp_f : weather.forecast[index].mintemp_c;
+        document.querySelector(`#${day}humidity`).innerHTML = weather.forecast[index].avghumidity + '%';
+        document.querySelector(`#${day}chanceofrain`).innerHTML = weather.forecast[index].chance_of_rain + '%';
+        document.querySelector(`#${day}precipitation`).innerHTML = weather.forecast[index].totalprecip_mm + 'mm';
+
+        document.querySelector(`#${day}sunrise`).innerHTML = weather.forecast[index].sunrise.toLocaleString('en-GB', {hourCycle: 'h12', hour12: true, timeStyle: 'short', timeZone: weather.location.tz});
+        document.querySelector(`#${day}sunset`).innerHTML = weather.forecast[index].sunset.toLocaleString('en-GB', {hourCycle: 'h12', hour12: true, timeStyle: 'short', timeZone: weather.location.tz});
+    }
+}
